80c31e8d517616c1e2a2c3a3f99cad2040a94ce5
Guvanch Yazgulyyev <77998139+GuvanchYazgulyyev@users.noreply.github.com>
2024-03-22 02:21:16 +0300

    Add project files.

---
+++ Haylou/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Haylou
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Haylou/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Haylou
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
